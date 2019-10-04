b42b19028ff99e5ff3f260bed2dc21f12a516c10
ludvigfranzen <franzen.ludvig@gmail.com>
2019-10-04 16:15:52 +0200

    js added

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+var galleryElems = document.querySelectorAll('.main-carousel');
+for ( var i=0, len = galleryElems.length; i < len; i++ ) {
+  var galleryElem = galleryElems[i];
+  new Flickity( galleryElem, {
+    // options...
+    cellAlign: 'left',
+  contain: true
+  });
+}
