e1797dba5b2f003d975de87089a32f004b502032
ludvigfranzen <franzen.ludvig@gmail.com>
2019-11-29 16:43:54 +0100

    per hack

--- js/main.js
+++ js/main.js
@@ -53,11 +53,13 @@ $carousel.on( 'staticClick.flickity', function( event, pointer, cellElement, cel
 
 
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
+let vh;
+document.addEventListener("DOMContentLoaded", function(){
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 // We listen to the resize event
 window.addEventListener('resize', () => {
   // We execute the same script as before
