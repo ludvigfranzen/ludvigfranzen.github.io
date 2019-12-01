8b5834753920cdccee73a42493be00c36be313f9
ludvigfranzen <franzen.ludvig@gmail.com>
2019-12-01 10:36:42 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -52,18 +52,3 @@ $carousel.on( 'staticClick.flickity', function( event, pointer, cellElement, cel
 });
 
 
-
-let vh;
-document.addEventListener("DOMContentLoaded", function(){
-  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-  vh = window.innerHeight * 0.01;
-  // Then we set the value in the --vh custom property to the root of the document
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-// We listen to the resize event
-window.addEventListener('resize', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
